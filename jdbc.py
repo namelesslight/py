0724831258ce9b2ececbb0d4b93d60fe37057681
zhaochanglang <1826992499@qq.com>
2021-10-22 12:56:44 +0800

    py

--- jdbc.py
+++ jdbc.py
@@ -6,41 +6,52 @@
 
 import pymysql
 db =pymysql.connect(host='localhost',port=3306,user='',passwd='',database='test')
+#连接数据库
 c=db.cursor()
+#添加
 try:
     c.execute("""
     insert into user values(1,'zcl')
     insert into user values(2,'zhaochanglang')
     """)
+    #执行，返回执行成功行数
     s=db.commit()
 except:
+    #失败回滚
     db.rollback()
     print("Error")
 else:
     print(s)
-
+#查询
 try:
+    #设置sql语句
     c.execute("""select * from people""")
+    #获取内容
     data=c.fetchall()
 except:
     print('Error')
 else:
     print(data)
-
+#修改
 try:
+    #设置sql语句
     c.execute("""update people set values(3,'zcl') where name='zcl'""")
+    #执行，返回执行成功行数
     s=db.commit()
 except:
+    #失败回滚
     db.rollback()
     print('Error')
 else:
     print(s)
-
-
+#删除
 try:
+    #设置sql语句
     c.excute("""delete from people where name='zcl'""")
-    s-db.commit()
+    #执行，返回执行成功行数
+    s=db.commit()
 except:
+    #失败回滚
     db.rollback()
     print('Error')
 else:
---
+++ thread_lock.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :thread_lock.py
+# @Time      :2021/10/19 14:21:07
+# @Author    :ZCL
+import threading
+import time
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print ("开启线程： " + self.name)
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        threadLock.release()
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+threadLock = threading.Lock()
+threads = []
+
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+thread1.start()
+thread2.start()
+
+threads.append(thread1)
+threads.append(thread2)
+
+for t in threads:
+    t.join()
+print ("退出主线程")
